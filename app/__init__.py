9a8c14c83ac8bad1c1895e05ebf03430caed6464
dannzii <34076867+dannzii@users.noreply.github.com>
2019-02-14 15:28:16 -0500

    removed password and username

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Dannzii'
 app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
 app.config['MAIL_PORT'] = '465'
-app.config['MAIL_USERNAME'] = 'bd52eee4d7df35'
-app.config['MAIL_PASSWORD'] = '5721143da6d507'
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
 
 mail = Mail(app)
 from app import views
